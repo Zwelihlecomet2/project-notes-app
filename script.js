f34e99a0883701d15cc0221b7b73287e5e82f260
unknown <202215849@smcsdom.local>
2024-07-24 12:22:48 +0200

    added assts file

--- script.js
+++ script.js
@@ -1,137 +1,140 @@
-// For the TimeStamps
-const timeStamp = document.getElementById("timeStamp");
-const updateTime = () =>{
-    const date = new Date();
-    const hour = date.getHours().toString().padStart(2,0);
-    const minute = date.getMinutes().toString().padStart(2,0);
-    const seconds = date.getSeconds().toString().padStart(2,0);
-    const currentTime = `${hour}:${minute}:${seconds}`;
-    timeStamp.textContent = `Edited ` + currentTime;
-}
+// // For the TimeStamps
+// const timeStamp = document.getElementById("timeStamp");
+// const updateTime = () =>{
+//     const date = new Date();
+//     const hour = date.getHours().toString().padStart(2,0);
+//     const minute = date.getMinutes().toString().padStart(2,0);
+//     const seconds = date.getSeconds().toString().padStart(2,0);
+//     const currentTime = `${hour}:${minute}:${seconds}`;
+//     timeStamp.textContent = `Edited ` + currentTime;
+// }
 
-updateTime();
-setInterval(updateTime, 1000);
+// updateTime();
+// setInterval(updateTime, 1000);
 
-const timeText = document.getElementById("timeText");
+// const timeText = document.getElementById("timeText");
 
-const updateDay = () =>{
-    const date = new Date();
-    const today = date.getDate();
-    let month = date.getMonth();
+// const updateDay = () =>{
+//     const date = new Date();
+//     const today = date.getDate();
+//     let month = date.getMonth();
     
-    switch(month){
-        case 0:
-            month = "Jan";
-            break;
+//     switch(month){
+//         case 0:
+//             month = "Jan";
+//             break;
         
-        case 1:
-            month = "Feb";
-            break;
+//         case 1:
+//             month = "Feb";
+//             break;
     
-        case 2:
-            month = "Mar";
-            break;
+//         case 2:
+//             month = "Mar";
+//             break;
     
-        case 3:
-            month = "Apr";
-            break;
+//         case 3:
+//             month = "Apr";
+//             break;
     
-        case 4:
-            month = "May";
-            break;    
+//         case 4:
+//             month = "May";
+//             break;    
             
-        case 5:
-            month = "Jun";
-            break;
+//         case 5:
+//             month = "Jun";
+//             break;
          
-        case 6:
-            month = "Jul";
-            break;
+//         case 6:
+//             month = "Jul";
+//             break;
     
-        case 7:
-            month = "Aug";
-            break;
+//         case 7:
+//             month = "Aug";
+//             break;
     
-        case 8:
-            month = "Sep";
-            break;
+//         case 8:
+//             month = "Sep";
+//             break;
     
-        case 9:
-            month = "Oct";
-            break;
+//         case 9:
+//             month = "Oct";
+//             break;
     
-        case 10:
-            month = "Nov";
-            break;
+//         case 10:
+//             month = "Nov";
+//             break;
     
-        case 11:
-            month = "Dec";
-            break;
+//         case 11:
+//             month = "Dec";
+//             break;
     
-        default:
-            month = "";
-    }
+//         default:
+//             month = "";
+//     }
 
-    const year = date.getFullYear();
+//     const year = date.getFullYear();
 
-    timeText.textContent = `Est ${today} ${month} ${year}`;
-};
+//     timeText.textContent = `Est ${today} ${month} ${year}`;
+// };
 
-updateDay();
-
-
-// The forms
-const firstForm = document.querySelector(".form-container");
-const secondForm = document.querySelector(".active-form");
-const closeForm = document.querySelector(".close-text");
-const noteTitle = document.querySelector("#note-title");
-const noteText = document.querySelector("#note-text");
-
-firstForm.addEventListener("click", () =>{
-    firstForm.style.visibility = "hidden";
-    secondForm.style.visibility = "visible";
-    noteText.focus();
-});
+// updateDay();
 
 
-closeForm.addEventListener("click", () =>{
-    firstForm.style.visibility = "visible";
-    secondForm.style.visibility = "hidden";
-    handleForms();
-    noteTitle.value = "";
-    noteText.value = "";
+// // The forms
+// const firstForm = document.querySelector(".form-container");
+// const secondForm = document.querySelector(".active-form");
+// const closeForm = document.querySelector(".close-text");
+// const noteTitle = document.querySelector("#note-title");
+// const noteText = document.querySelector("#note-text");
 
-});
-
-
-const handleForms = () =>{
-        if(noteText.value !== ""){
-            console.log(noteText.value);
-        }
-};
+// firstForm.addEventListener("click", () =>{
+//     firstForm.style.visibility = "hidden";
+//     secondForm.style.visibility = "visible";
+//     noteText.focus();
+// });
 
 
+// closeForm.addEventListener("click", () =>{
+//     firstForm.style.visibility = "visible";
+//     secondForm.style.visibility = "hidden";
+//     handleForms();
+//     noteTitle.value = "";
+//     noteText.value = "";
 
-// To Be Replaced
-const modalOpening = document.getElementById("modalOpening");
-const note = document.getElementById("note");
-const close = document.getElementById("close");
-const sideBar = document.getElementById("sideBar");
-modalOpening.style.visibility = "hidden";   
+// });
 
-note.addEventListener("click", () =>{
-    modalOpening.style.visibility = "visible";
 
-    if(secondForm.style.visibility === "visible"){
-        firstForm.style.visibility = "visible";
-        secondForm.style.visibility = "hidden";
-    }
+// const handleForms = () =>{
+//         if(noteText.value !== ""){
+//             console.log(noteText.value);
+//         }
+// };
 
-    else{
-        return "";
-    }
-});
 
-close.addEventListener("click", () =>{
-    modalOpening.style.visibility = "hidden";
-});
+
+// // To Be Replaced
+// const modalOpening = document.getElementById("modalOpening");
+// const note = document.getElementById("note");
+// const close = document.getElementById("close");
+// const sideBar = document.getElementById("sideBar");
+// modalOpening.style.visibility = "hidden";   
+
+// note.addEventListener("click", () =>{
+//     modalOpening.style.visibility = "visible";
+
+//     if(secondForm.style.visibility === "visible"){
+//         firstForm.style.visibility = "visible";
+//         secondForm.style.visibility = "hidden";
+//     }
+
+//     else{
+//         return "";
+//     }
+// });
+
+// close.addEventListener("click", () =>{
+//     modalOpening.style.visibility = "hidden";
+// });
+
+
+
